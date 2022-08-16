96418a617274859246f5b71d1cdb764995a0b306
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2022-08-16 13:32:02 +0530

    Time: 4 ms (25.64%), Space: 6.1 MB (85.07%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        map<char,int>mp;
-        for(int i=0; i<s.length(); i++){
-            mp[s[i]]++;
-            if(mp[s[i]]==2) return s[i];
+        vector<int>v(26,0);
+        for(auto i:s){
+            cout<<i-'a'<<endl;
+            v[i-'a']++;
+            if(v[i-'a']==2)
+                return i;
         }
-        return -1;
+        return ' ';
+        //Approach -2
+        // map<char,int>mp;
+        // for(int i=0; i<s.length(); i++){
+        //     mp[s[i]]++;
+        //     if(mp[s[i]]==2) return s[i];
+        // }
+        // return -1;
     }
 };
