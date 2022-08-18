b1f35524ca9f4f8f627f940d76b6a4e4fb3735f2
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2022-08-18 15:34:51 +0530

    Time: 553 ms (11.51%), Space: 80.2 MB (31.31%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         int n=arr.size();
-        vector<int>vec;
+        // Appraoch 1 :vector<int>vec;
+        priority_queue<int>vec;
         map<int,int> mp;
         for(int i=0 ; i < n ; i++){
             mp[arr[i]]++;
         }
         for( auto x: mp){
-            vec.push_back(x.second);
+            vec.push(x.second);
         }
-        sort(vec.begin(),vec.end(),greater<int>());
-        for(auto x:vec) cout<<x<<endl;
+       // sort(vec.begin(),vec.end(),greater<int>());
+        //for(auto x:vec) cout<<x<<endl;
         int sum=0,c=0;
-        for(int i=0;i<vec.size();i++){
-            sum+=vec[i];
+         while(!vec.empty()){
+            sum+=vec.top();
+             vec.pop();
             if(sum>=n/2){
                 return c+1;
             }
             else{
                 c++;
             }
-            
         }
         return c;
     }
