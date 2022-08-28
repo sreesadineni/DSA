87d00036ad967a86227afbf58552b74ee76113f8
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2022-08-28 18:40:24 +0530

    Time: 15 ms (69.45%), Space: 9.6 MB (16.11%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-   vector<int>diagonal(int i,int j,int n, int c,vector<vector<int>>& mat){
-        vector<int>v;
-        while(i<n && j<c){
-            v.push_back(mat[i][j]);
-                i++;j++;
-        }
-        for(auto x:v) cout<<x<<" ";
-       sort(v.begin(),v.end());
-      
-       return v;
-    }
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int c=mat[0].size();
-        vector<vector<int>> m=mat;
-        vector<int>v;
-        for(int i=0 ; i<c ; i++){
-            int r=0,c1=i;
-            v=diagonal(0,i,n,c,mat);
-            int  cn=v.size(),j=0;      
-            while(r<n && c1<c && j<cn){
-                m[r][c1]=v[j];
-                r++;c1++;j++;
+       map<int, priority_queue<int,vector<int>,greater<int>>> mp;//min priority queue
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mp[i-j].push(mat[i][j]);   //diagonal elements have same (i-j) value
             }
         }
-        for(int i=1 ; i<n-1 ; i++){
-             int r=i,c1=0;
-            v=diagonal(i,0,n,c,mat);
-            int  cn=v.size(),j=0;      
-            while(r<n && c1<c && j<cn){
-                m[r][c1]=v[j];
-                r++;c1++;j++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = mp[i-j].top();
+                mp[i-j].pop();
             }
         }
-        return m;
+        return mat;
+       
     }
 };
