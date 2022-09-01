04cd473084879a53ead5fb23a6ef075f31151989
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2022-09-01 22:51:28 +0530

    Time: 38 ms, Memory: 8.1 MB - LeetHub

---
+++ roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+       int romanToInt(string s) {
+        map<char,int> mp;
+           mp.insert({'I',1});
+mp.insert({'V',5});
+mp.insert({'X',10});
+mp.insert({'L',50});
+mp.insert({'C',100});
+mp.insert({'D',500});
+mp.insert({'M',1000});
+
+
+    int num=0;
+    
+    for(int i=0;i<s.length()-1;i++)
+    {
+        if(mp[s[i]]<mp[s[i+1]])
+        {
+            num-=mp[s[i]];
+        }
+        else if(mp[s[i]]>=mp[s[i+1]])
+        {
+            num+=mp[s[i]];
+        }
+    }
+    num+=mp[s[s.length()-1]];
+    return num;   
+    }
+};
