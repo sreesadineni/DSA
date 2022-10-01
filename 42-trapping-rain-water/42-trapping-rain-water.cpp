031ebdf2ba44a0e1bba518fdd9047aee0c55a7c5
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2022-10-01 22:52:09 +0530

    Time: 47 ms (10.23%), Space: 20.5 MB (37.83%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    vector<int> NLIR(vector<int>& h){
-        vector<int>v(h.size());
-        v[0]=h[0];
-        for(int i=1 ; i<h.size() ; i++){
-            v[i]=max(h[i],v[i-1]);
+  int trap(vector<int>& height)
+  {
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
         }
-        return v;
+        st.push(current++);
     }
-    vector<int> NLIL(vector<int>& h){
-        vector<int>v(h.size());
-        v[h.size()-1]=h[h.size()-1];
-        for(int i=h.size()-2 ; i>=0 ;i--){
-           v[i]=max(h[i],v[i+1]);
-        }
-        return v;
-    }
-    int trap(vector<int>& h) {
-        vector<int>v1,v;
-        v1=NLIR(h);v=NLIL(h);
-        int ans=0;
-         for(int i=0 ;i<h.size();i++){
-            ans+=(min(v[i],v1[i])-h[i]);
-        }
-        return ans;
-    }
+    return ans;
+}
 };
