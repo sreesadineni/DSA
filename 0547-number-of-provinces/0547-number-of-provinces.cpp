320145215f0530c13175ffc87fac5922d5b3d7f7
Sree_Sadineni <59657254+sreesadineni@users.noreply.github.com>
2023-06-04 14:08:07 +0530

    Time: 34 ms (18.19%), Space: 14.9 MB (37.02%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -19,12 +19,6 @@ public:
                 }
             }
         }
-        for(auto x:mp){
-            cout<<x.first<<" ";
-            for(auto c:x.second){
-                cout<<c<<"  ";
-            }
-        }
         int vis[201]={0};
         int cnt=0;
         for(int i=0 ; i<n ;i++){
