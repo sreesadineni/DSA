596d1a22f3571b3dfaeb14bb6285eb82f6bc5591
Sadineni Sree <sreesadineni@gmail.com>
2023-10-10 17:55:04 +0530

    Time: 3 ms (31.45%), Space: 6.5 MB (11.86%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int>v;
         for(int i=1;i<=n;i++){
             if(n%i == 0){
-                v.push_back(i);
+                k--;
+            }
+            if(k==0){
+                return i;
             }
         }
-        if(v.size()<k) return -1;
-        return v[k-1];
+        return -1;
     }
 };
